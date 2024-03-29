8372307990c0003ed3b972d06370fe681c392585
Bipul Kumar <bknor951@gmail.com>
2024-03-30 00:32:45 +0530

    stack-navigation

---
+++ screens/HomeScreen.js
@@ -0,0 +1,101 @@
+/* eslint-disable no-shadow */
+/* eslint-disable no-unused-vars */
+/* eslint-disable react-native/no-inline-styles */
+import {Pressable, ScrollView, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import moment from 'moment';
+
+const HomeScreen = () => {
+  const currentDate = moment();
+  const startOfWeek = currentDate.clone().startOf('week');
+  const [date, setDate] = useState('');
+  const [nextDate, setNextDate] = useState('');
+  const [selectedDate, setSelectedDate] = useState('');
+
+  const renderWeekDates = startOfWeek => {
+    let weekDates = [];
+    for (let i = 0; i < 7; i++) {
+      const date = startOfWeek.clone().add(i, 'days');
+
+      const formattedDate = date.format('ddd DD');
+
+      const isCurrentDate = date.isSame(currentDate, 'day');
+
+      weekDates.push(
+        <View style={{flexDirection: 'row', gap: 12, marginVertical: 10}}>
+          <View
+            style={[
+              {
+                height: 40,
+                width: 40,
+                borderRadius: 20,
+                backgroundColor: 'white',
+                marginVertical: 10,
+                justifyContent: 'center',
+                alignItems: 'center',
+              },
+              isCurrentDate && {backgroundColor: 'black'},
+            ]}>
+            <Text
+              style={{
+                fontSize: 11,
+                fontWeight: 500,
+                color: isCurrentDate ? 'white' : 'black',
+              }}>
+              {date.format('DD')}
+            </Text>
+            <Text
+              style={{
+                fontSize: 11,
+                fontWeight: 500,
+                color: isCurrentDate ? 'white' : 'black',
+              }}>
+              {date.format('ddd')}
+            </Text>
+          </View>
+
+          <Pressable
+            style={{
+              backgroundColor: 'white',
+              borderRadius: 8,
+              padding: 10,
+              width: '85%',
+              height: 80,
+            }}>
+            <Text>There is no menu</Text>
+          </Pressable>
+        </View>,
+      );
+
+      return weekDates;
+    }
+
+    const renderWeeks = numWeeks => {
+      let weeks = [];
+      for (let i = 0; i < numWeeks; i++) {
+        weeks.push(
+          <View>
+            <Text>
+              {startOfWeek
+                .clone()
+                .add(i * 7, 'days')
+                .format('DD MMM')}
+            </Text>
+            <Text>
+              {renderWeekDates(startOfWeek.clone().add(i * 7, 'days'))}
+            </Text>
+          </View>,
+        );
+      }
+      return weeks;
+    };
+
+    return (
+      <ScrollView style={{marginTop: 50}}>
+        <View style={{flex: 1, padding: 12}}>{renderWeeks(3)}</View>
+      </ScrollView>
+    );
+  };
+};
+
+export default HomeScreen;
